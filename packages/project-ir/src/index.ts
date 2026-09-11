2513dcf04bb763f28645ee6f262ed7549922dfe1
inlifemedicina <inlifemedicina@gmail.com>
2026-09-11 15:51:02 -0300

    feat(project-ir): export package API

---
+++ packages/project-ir/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./validation.js";
+export * from "./factory.js";
+export * from "./migrations.js";
+export * from "./commands.js";
+export * from "./history.js";
