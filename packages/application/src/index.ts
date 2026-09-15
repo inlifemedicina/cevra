1d76e885dfb31a7bba68a07ba13a0f2a28ea1616
inlifemedicina <inlifemedicina@gmail.com>
2026-09-15 12:59:01 -0300

    feat: promote aligned transcripts through project history

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -3,4 +3,5 @@ export * from "./local-source-ingest.js";
 export * from "./media-service.js";
 export * from "./repository.js";
 export * from "./transcription-service.js";
+export * from "./alignment-service.js";
 export * from "./types.js";
